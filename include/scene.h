9b9b77b1f060dd376737f90b119ad9c4c644b0f5
Sahil <ssfaruque1@gmail.com>
2018-10-10 01:33:33 -0700

    working on scene

--- include/scene.h
+++ include/scene.h
@@ -1,13 +1,24 @@
 #ifndef SCENE_H
 #define SCENE_H
 
+#include <vector>
 
+#include "entity.h"
 
 class Scene
 {
     private:
+        std::vector<Entity*> m_entities;
 
     public:
+        Scene();
+        ~Scene();
+
+        void addEntity(Entity* entity);
+        void draw();
+        void displayEntities() const;
+
+        
 
 };
 
---
+++ src/scene.cpp
@@ -0,0 +1,28 @@
+#include "scene.h"
+
+Scene::Scene()
+{
+
+}
+
+Scene::~Scene()
+{
+
+}
+
+void Scene::addEntity(Entity* entity)
+{
+
+}
+
+
+void Scene::draw()
+{
+
+}
+
+
+void Scene::displayEntities() const
+{
+
+}
